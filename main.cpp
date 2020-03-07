906e7118d28f5fec75bd370eff9460ae1f52718d
MDm1try <dima.miroshn4enko@gmail.com>
2020-03-07 13:58:03 +0200

    Added pow function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+
+int pow (int a) {
+    return a * a;
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     // hello world
+    int a = pow(10);
     return a.exec();
 }
