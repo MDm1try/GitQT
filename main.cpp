2215f257646abe3c22c61729a64a91f6170cfff5
MDm1try <dima.miroshn4enko@gmail.com>
2020-05-10 21:42:51 +0300

    Add min funnction

--- main.cpp
+++ main.cpp
@@ -16,6 +16,15 @@ int max(int x, int y)
       return y;
 }
 
+int min(int x, int y)
+{
+    if (x > y)
+      return y;
+    else
+      return x;
+}
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
